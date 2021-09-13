9f1f64f2957a98f5b60f7a4500f12806cac56d1d
mahipalcoding <86092098+mahipalcoding@users.noreply.github.com>
2021-09-13 12:58:08 +0530

    new

--- js/js.js
+++
@@ -1,9 +0,0 @@
-function loader(){
-    document.querySelector('.loader-container').classList.add('fade-out');
-  }
-  
-  function fadeOut(){
-    setInterval(loader, 2000);
-  }
-  
-  window.onload = fadeOut;
